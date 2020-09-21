ccfe5259ee2c55992f43580e921f06c38ee0e14a
Carlos Barrios <juanbarrios045@gmail.com>
2020-09-21 16:59:16 -0400

    Master | Fix webpack bug

--- webpack/production.js
+++ webpack/production.js
@@ -1,38 +1,38 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
 const rules = {
-  babel: require("./rules/babel"),
-  html: require("./rules/html"),
-  fonts: require("./rules/fonts"),
-  images: require("./rules/images"),
+  babel: require('./rules/babel'),
+  html: require('./rules/html'),
+  fonts: require('./rules/fonts'),
+  images: require('./rules/images'),
 };
 
 const loaders = {
-  css: require("./loaders/css"),
-  sass: require("./loaders/sass"),
+  css: require('./loaders/css'),
+  sass: require('./loaders/sass'),
 };
 
 const plugins = {
-  html: require("./plugins/html"),
-  favicons: require("./plugins/favicons"),
+  html: require('./plugins/html'),
+  favicons: require('./plugins/favicons'),
 };
 
 const config = {
-  entry: path.resolve(__dirname, "../", "./src/index.js"),
+  entry: path.resolve(__dirname, '../', './src/index.js'),
 
   output: {
-    filename: "javascripts/[name].[hash].js",
-    chunkFilename: "javascripts/[name].[hash].chunk.js",
-    publicPath: "./",
+    filename: 'javascripts/[name].[hash].js',
+    chunkFilename: 'javascripts/[name].[hash].chunk.js',
+    publicPath: '/dist/',
   },
 
-  mode: "production",
+  mode: 'production',
   module: {
     rules: [
       rules.babel,
@@ -41,14 +41,14 @@ const config = {
       rules.images,
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(scss|sass)$/i,
         use: [
           MiniCssExtractPlugin.loader,
           loaders.css,
-          "resolve-url-loader",
+          'resolve-url-loader',
           loaders.sass,
         ],
       },
@@ -56,14 +56,14 @@ const config = {
   },
 
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
 
   plugins: [
     plugins.html,
     plugins.favicons,
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname, ".."),
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '..'),
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
@@ -71,7 +71,7 @@ const config = {
     new LodashModuleReplacementPlugin(),
     new UglifyJSPlugin({}),
     new MiniCssExtractPlugin({
-      filename: "stylesheets/[name].[hash].css",
+      filename: 'stylesheets/[name].[hash].css',
       allChunks: true,
     }),
   ],
